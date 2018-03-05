70f95577f8c4081fab6f65ead37b19dcaf97740e
Timothy Ting <timothy@tune.com>
2018-03-05 10:26:04 -0800

    See errors that our knn commits

--- knn.py
+++ knn.py
@@ -61,35 +61,73 @@ labels = labeled_images.iloc[0:5000,:1]
 # plt.show()
 
 # Binary cutoff point for image pixels vs percentage correct
-x = []
-y = []
-cutoff_points = [x / 10.0 for x in range(0, 10)]
-for cutoff_point in cutoff_points:
-    scores = []
+# x = []
+# y = []
+# cutoff_points = [x / 20.0 for x in range(0, 20)]
+# for cutoff_point in cutoff_points:
+#     scores = []
 
-    for _ in range(3):
-        train_images, test_images, train_labels, test_labels = train_test_split(images, labels, train_size=1000)
+#     for _ in range(5):
+#         train_images, test_images, train_labels, test_labels = train_test_split(images, labels, train_size=1000)
 
-        train_images[train_images > cutoff_point] = 1
-        test_images[test_images > cutoff_point] = 1
-        train_images[train_images <= cutoff_point] = 0
-        test_images[test_images <= cutoff_point] = 0
+#         train_images[train_images > cutoff_point] = 1
+#         test_images[test_images > cutoff_point] = 1
+#         train_images[train_images <= cutoff_point] = 0
+#         test_images[test_images <= cutoff_point] = 0
 
-        knn = neighbors.KNeighborsClassifier(p=1, n_neighbors=3)
-        print("loading data")
-        knn.fit(train_images, train_labels.values.ravel())
+#         knn = neighbors.KNeighborsClassifier(p=1, n_neighbors=3)
+#         print("loading data")
+#         knn.fit(train_images, train_labels.values.ravel())
 
-        score = knn.score(test_images, test_labels.values.ravel())
-        scores.append(score)
+#         score = knn.score(test_images, test_labels.values.ravel())
+#         scores.append(score)
 
-    average_score = sum(scores) / len(scores)
-    x.append(cutoff_point)
-    y.append(average_score)
+#     average_score = sum(scores) / len(scores)
+#     x.append(cutoff_point)
+#     y.append(average_score)
 
-plt.plot(x, y)
-plt.ylabel("Percentage correct in test set")
-plt.xlabel("Cutoff Point")
-plt.show()
+# plt.plot(x, y)
+# plt.ylabel("Percentage correct in test set")
+# plt.xlabel("Cutoff Point")
+# plt.show()
+
+scores = []
+
+train_images, test_images, train_labels, test_labels = train_test_split(images, labels, train_size=1000)
+
+train_images[train_images > 0.3] = 1
+test_images[test_images > 0.3] = 1
+train_images[train_images <= 0.3] = 0
+test_images[test_images <= 0.3] = 0
+
+knn = neighbors.KNeighborsClassifier(p=1, n_neighbors=3)
+print("loading data")
+knn.fit(train_images, train_labels.values.ravel())
+
+actuals = test_labels.values.ravel()
+predictions = knn.predict(test_images)
+for idx, prediction in enumerate(predictions):
+    if prediction != actuals[idx]:
+        fig=plt.figure(figsize=(8, 8))
+        test_img = test_images.iloc[idx].as_matrix()
+        test_img = test_img.reshape((28,28))
+        fig.add_subplot(2, 3, 2)
+        plt.imshow(test_img,cmap='gray')
+        plt.title("pred: %s, actual: %s" % (prediction, actuals[idx]))
+        nn_indices = knn.kneighbors([test_images.iloc[idx]], 3, False)
+        print(idx)
+        print(nn_indices)
+        for j, nn_index in enumerate(nn_indices[0]):
+            train_img = train_images.iloc[nn_index].as_matrix()
+            train_img = train_img.reshape((28,28))
+            fig.add_subplot(2, 3, j + 4)
+            plt.imshow(train_img,cmap='gray')
+            plt.title(train_labels.iloc[nn_index,0])
+        plt.show()
+        input()
+        #print("test[%d] indices: %s" % (idx, ", ".join(nn_indices[0])))
+
+
 
 # Vary number of neighbors
 # Vary cutoff/granularity
